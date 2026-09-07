ee9b398257e1f9a3e659fadb01fe45d1985ba9b3
hellobbn <hellobbn@users.noreply.github.com>
2026-09-08 00:53:55 +0800

    release: prepare 2.1.61 with version code 95

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         manifestPlaceholders["appLabel"] = "@string/app_name"
         minSdk = 26
         targetSdk = 36
-        versionCode = 94
-        versionName = "2.1.61-beta"
+        versionCode = 95
+        versionName = "2.1.61"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val commitCount = gitCommitCount()
         val sha = gitSha()
